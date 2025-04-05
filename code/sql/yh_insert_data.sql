c66c9874942600c13e1da04bb29d69b79fddaf9b
h4zan <h4zan@hotmail.com>
2025-04-05 15:28:54 +0200

    insert and query data

---
+++ code/sql/yh_insert_data.sql
@@ -0,0 +1,272 @@
+SET
+    search_path TO yrkesco_data;
+
+INSERT INTO
+    address (address, zip_code, locality, city)
+VALUES
+    ('Kungsgatan 1', '11120', 'Norrmalm', 'Stockholm'),
+    ('Avenyn 10', '41136', 'Centrum', 'Göteborg'),
+    ('Storgatan 5', '75220', 'Fålhagen', 'Uppsala'),
+    ('Drottninggatan 15', '21145', 'Centrum', 'Malmö'),
+    ('Rådhusesplanaden 2', '90328', 'Centrum', 'Umeå'),
+    ('Sveavägen 12', '11357', 'Vasastan', 'Stockholm'),
+    (
+        'Linnégatan 25',
+        '41304',
+        'Linnéstaden',
+        'Göteborg'
+    ),
+    (
+        'Kungsängsgatan 18',
+        '75322',
+        'Fålhagen',
+        'Uppsala'
+    ),
+    ('Amiralsgatan 20', '21155', 'Centrum', 'Malmö'),
+    ('Skolgatan 7', '90327', 'Öst på stan', 'Umeå'),
+    ('Odengatan 11', '11424', 'Vasastan', 'Stockholm'),
+    ('Vasagatan 8', '41124', 'Vasastan', 'Göteborg'),
+    ('Vaksalagatan 1', '75320', 'Fålhagen', 'Uppsala'),
+    (
+        'Regementsgatan 5',
+        '21753',
+        'Slottsstaden',
+        'Malmö'
+    ),
+    ('Kungsgatan 12', '11121', 'Norrmalm', 'Stockholm');
+
+INSERT INTO
+    personalInfo (
+        personal_identification_number,
+        emergency_contact_info,
+        emergency_contatact_number
+    )
+VALUES
+    ('19900101-1234', 'Anna Svensson', '0701234567'),
+    ('19850515-5678', 'Johan Karlsson', '0739876543'),
+    ('19991212-9999', 'Lisa Persson', '0762345678'),
+    ('20010303-3333', 'Erik Larsson', '0723456789'),
+    ('19681201-4444', 'Sara Kent', '0723444556');
+
+INSERT INTO
+    vocationalUniversity (title, establishment_date)
+VALUES
+    ('YrkesCo Stockholm', '2015-08-01'),
+    ('YrkesCo Göteborg', '2017-01-15'),
+    ('YrkesCo Uppsala', '2023-02-01'),
+    ('YrkesCo Malmö', '2022-09-01'),
+    ('YrkesCo Umeå', '2020-04-01');
+
+INSERT INTO
+    vuLocation (vocational_university_id, address_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5);
+
+INSERT INTO
+    student (
+        first_name,
+        last_name,
+        email,
+        personal_info_id,
+        address_id
+    )
+VALUES
+    ('Sara', 'Lind', 'sara.lind@example.com', 1, 6),
+    ('Erik', 'Berg', 'erik.berg@example.com', 2, 7),
+    (
+        'Maja',
+        'Nilsson',
+        'maja.nilsson@example.com',
+        3,
+        8
+    ),
+    ('Oscar', 'Ek', 'oscar.ek@example.com', 4, 9);
+
+INSERT INTO
+    employee (
+        first_name,
+        last_name,
+        hire_date,
+        resignation_date,
+        profession,
+        employment_type,
+        address_id,
+        personal_info_id,
+        vu_location_id
+    )
+VALUES
+    (
+        'Maria',
+        'Ek',
+        '2020-01-01',
+        NULL,
+        'Instructor',
+        'Full-time',
+        11,
+        1,
+        1
+    ),
+    (
+        'David',
+        'Holm',
+        '2021-06-15',
+        NULL,
+        'Program Director',
+        'Full-time',
+        12,
+        2,
+        2
+    ),
+    (
+        'Jenny',
+        'Larsen',
+        '2019-03-20',
+        NULL,
+        'Instructor',
+        'Part-time',
+        13,
+        3,
+        3
+    ),
+    (
+        'Tomas',
+        'Frisk',
+        '2018-11-11',
+        NULL,
+        'Instructor',
+        'Full-time',
+        14,
+        4,
+        4
+    ),
+    (
+        'Hanna',
+        'West',
+        '2016-12-21',
+        NULL,
+        'Program Director',
+        'Full-time',
+        15,
+        5,
+        5
+    );
+
+INSERT INTO
+    instructor (employee_id)
+VALUES
+    (1),
+    (3),
+    (4);
+
+INSERT INTO
+    programDirector (employee_id)
+VALUES
+    (2),
+    (5);
+
+INSERT INTO
+    subject (subject_name, subject_area)
+VALUES
+    ('Programming', 'IT'),
+    ('Databases', 'IT'),
+    ('Management', 'Business'),
+    ('WebDev', 'IT');
+
+INSERT INTO
+    course (
+        course_name,
+        course_code,
+        point,
+        status,
+        subject_id,
+        course_description,
+        is_independent_course
+    )
+VALUES
+    (
+        'Python',
+        'PY101',
+        '15',
+        'Active',
+        1,
+        'Intro to Python.',
+        false
+    ),
+    (
+        'SQL',
+        'DB202',
+        '20',
+        'Active',
+        2,
+        'Learn databases.',
+        true
+    ),
+    (
+        'HTML and CSS',
+        'WEB101',
+        '10',
+        'Active',
+        4,
+        'Frontend basics.',
+        false
+    ),
+    (
+        'Agile Project',
+        'PM301',
+        '15',
+        'Active',
+        3,
+        'Scrum and Kanban.',
+        false
+    );
+
+INSERT INTO
+    program (program_name, program_year, status)
+VALUES
+    ('SoftwareDev', 2023, 'Active'),
+    ('DataScience', 2023, 'Active'),
+    ('WebDesign', 2024, 'Planned'),
+    ('ITProjectMgmt', 2024, 'Active');
+
+INSERT INTO
+    programCourse (course_id, program_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4);
+
+INSERT INTO
+    class (
+        class_label,
+        class_grade,
+        class_year,
+        course_id,
+        vu_location_id,
+        program_director_id
+    )
+VALUES
+    ('sd23', 'A', 2023, 1, 1, 1),
+    ('ds23', 'B', 2023, 2, 2, 1),
+    ('wd24', 'C', 2024, 3, 3, 1),
+    ('pm24', 'D', 2024, 4, 4, 2);
+
+INSERT INTO
+    enrollment (status, student_id, class_id)
+VALUES
+    ('Active', 1, 1),
+    ('Completed', 2, 2),
+    ('Active', 3, 3),
+    ('Active', 4, 4);
+
+INSERT INTO
+    classInstructor (class_id, instructor_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 2);
---
+++ code/sql/yh_query_data.sql
@@ -0,0 +1,60 @@
+SET search_path TO yrkesco_data;
+
+SELECT * FROM yrkesco_data.course;
+
+SELECT 
+  COUNT(*) AS total_courses
+FROM 
+  yrkesco_data.course
+WHERE is_independent_course = TRUE;
+
+
+SELECT 
+  co.course_name,
+  COUNT(c.id) AS number_of_classes
+FROM 
+   yrkesco_data.course co
+LEFT JOIN 
+   yrkesco_data.class c ON c.course_id = co.id
+GROUP BY 
+  co.course_name;
+
+SELECT 
+  c.class_label, 
+  CONCAT(e.first_name, ' ', e.last_name) AS program_director_name
+FROM 
+  yrkesco_data.class c
+JOIN 
+  yrkesco_data.programdirector pd ON c.program_director_id = pd.id
+JOIN 
+  yrkesco_data.employee e ON pd.employee_id = e.id
+WHERE 
+  c.class_label = 'sd23';
+
+
+SELECT 
+  c.class_label,
+  CONCAT(e.first_name, ' ', e.last_name) AS instructor_name
+FROM 
+  yrkesco_data.classInstructor ci
+JOIN 
+  yrkesco_data.class c ON ci.class_id = c.id
+JOIN 
+  yrkesco_data.instructor i ON ci.instructor_id = i.id
+JOIN 
+  yrkesco_data.employee e ON i.employee_id = e.id
+ORDER BY 
+  c.class_label;
+
+SELECT 
+  CONCAT(e.first_name, ' ', e.last_name) AS program_director_name,
+  c.class_label
+FROM 
+  yrkesco_data.programdirector pd
+JOIN 
+  yrkesco_data.employee e ON pd.employee_id = e.id
+JOIN 
+  yrkesco_data.class c ON pd.id = c.program_director_id
+ORDER BY 
+  program_director_name, 
+  c.class_label;
